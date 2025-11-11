ecd6fd016418152f2bc10b1aad981b410b175bc3
robertopfneto <neto2014roberto@gmail.com>
2025-11-11 12:55:41 -0300

    inserindo cadastro e ajustando autenticação

--- api_auth_node/server.js
+++ api_auth_node/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './mongoDb.js';
 import publicRoutes from './routes/auth_routes.js';
@@ -6,6 +7,7 @@ import publicRoutes from './routes/auth_routes.js';
 dotenv.config();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use('/usuarios', publicRoutes);
 
